e5b2b27584d5772d09b0a05fbe8df143deffe0b7
S. Grimault <sebastien.grimault@gmail.com>
2020-06-07 15:45:18 +0200

    chore: update core module dependency

--- upgrade_submodules.sh
+++ upgrade_submodules.sh
@@ -2,13 +2,8 @@
 
 APP_HOME="`pwd -P`"
 
-cd $APP_HOME/mountpoint
-git checkout -- .
-
-cd $APP_HOME/gn_mobile_core
-git pull origin develop
-
-cd $APP_HOME
+git submodule foreach git reset --hard HEAD
+git submodule foreach git clean -f
 git submodule update --remote
 git submodule foreach git checkout develop
 git submodule foreach git pull origin develop
